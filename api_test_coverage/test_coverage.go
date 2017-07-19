b6bc8187d7ae9d48b0b41b8f0fc23909942daaf5
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-07-18 18:31:50 -0700

    Update for parsing json

--- api_test_coverage/test_coverage.go
+++ api_test_coverage/test_coverage.go
@@ -8,10 +8,20 @@ import (
 )
 
 func main() {
-	fmt.Println("hello world")
-}
+	var decode_data interface{}
 
-func isParameter(b byte) {
 	fmt.Println("hello world")
-}
 
+	bytes, err := ioutil.ReadFile("swagger.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.Unmarshal(bytes, &decode_data); err != nil {
+		log.Fatal(err)
+	}
+	for key, data := range decode_data.(map[string]interface {}) {
+		var d = data.(string)
+		fmt.Printf("%s: %s\n", key, d)
+		break
+	}
+}
