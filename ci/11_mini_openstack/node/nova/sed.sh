90c33a1d9afd03b249daea8e70b8f30cbf4a74a4
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-10-10 13:14:12 -0700

    Fix sed.sh

--- ci/11_mini_openstack/node/nova/sed.sh
+++ ci/11_mini_openstack/node/nova/sed.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-shell: MY_IP=`ifconfig | grep "inet addr:192.168" | awk '{print $2}' | awk -F: '{print $2}'`
+MY_IP=`ifconfig | grep "inet addr:192.168.1." | awk '{print $2}' | awk -F: '{print $2}'`
 sed -i s/"my_ip = 192.168.1.100"/"my_ip = ${MY_IP}"/ /etc/nova/nova.conf
