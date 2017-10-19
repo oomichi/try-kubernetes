1ce4d9fee27bed17253c926360985dbdb0a52ac6
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-10-19 13:54:55 -0700

    Add OpenStack operation on CI

--- ci/00_gitlab-runner/ci_test/test_script.sh
+++ ci/00_gitlab-runner/ci_test/test_script.sh
@@ -6,6 +6,8 @@ LAST_COMMIT=`echo ${LAST_LINE} | awk -F "/" '{print $NF}'`
 GIT_URL=`echo ${LAST_LINE} | sed s@"/commit/${LAST_COMMIT}"@@`
 GIT_DIRNAME=`echo ${GIT_URL} | awk -F "/" '{print $NF}'`
 
+SECGROUP="cffa06fb-b436-4fa1-be6c-e9d7ffa4d476"
+
 echo "LAST_COMMIT: $LAST_COMMIT"
 echo "GIT_URL: $GIT_URL"
 echo "GIT_DIRNAME: $GIT_DIRNAME"
@@ -23,7 +25,35 @@ if [ -z ${GIT_DIRNAME} ]; then
 	exit 1
 fi
 
+# Clone remora git ASAP because the following operation takes much time and need to avoid conflict another commits
 git clone ${GIT_URL}
+
+# Create virtual machines with OpenStack
+source ./adminrc
+NETID=`openstack network show -c id -f value provider`
+MASTER=`openstack server create -c id -f value --flavor m1.medium --image Ubuntu-16.04-x86_64 --nic net-id=${NETID} --security-group ${SECGROUP} --key-name mykey master`
+if [ $? -ne 0 ]; then
+	echo "Failed to create a virtual machine for master"
+	exit 1
+fi
+WORKER01=`openstack server create -c id -f value --flavor m1.medium --image Ubuntu-16.04-x86_64 --nic net-id=${NETID} --security-group ${SECGROUP} --key-name mykey worker01`
+if [ $? -ne 0 ]; then
+	echo "Failed to create a virtual machine for worker01"
+	exit 1
+fi
+WORKER02=`openstack server create -c id -f value --flavor m1.medium --image Ubuntu-16.04-x86_64 --nic net-id=${NETID} --security-group ${SECGROUP} --key-name mykey worker02`
+if [ $? -ne 0 ]; then
+	echo "Failed to create a virtual machine for worker02"
+	exit 1
+fi
+
+sleep 10
+
+# Know ip addresses of virtual machines
+IP_MASTER=`openstack server show -c addresses -f value ${MASTER} | sed s/'provider='//`
+IP_WORKER01=`openstack server show -c addresses -f value ${WORKER01} | sed s/'provider='//`
+IP_WORKER02=`openstack server show -c addresses -f value ${WORKER02} | sed s/'provider='//`
+
 cd ./${GIT_DIRNAME}
 
 # It is possible that the latest commit of the target repo is different from LAST_COMMIT
