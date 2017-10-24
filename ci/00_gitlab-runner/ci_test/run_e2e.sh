19576adbada6c3d19fd59894aa780b31fdd805b2
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-10-24 11:09:45 -0700

    Add progress messages

--- ci/00_gitlab-runner/ci_test/run_e2e.sh
+++ ci/00_gitlab-runner/ci_test/run_e2e.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+echo "Start to create e2e test env.."
 ansible-playbook -vvvv -i ./hosts_vms create_e2e_test_env.yaml
 if [ $? -ne 0 ]; then
 	echo "Failed to create e2e test env"
 	exit 1
 fi
 
+echo "Start to run e2e test.."
 ansible-playbook -vvvv -i ./hosts_vms run_e2e_test.yaml
 if [ $? -ne 0 ]; then
 	echo "Failed to run e2e test"
 	exit 1
 fi
+
+echo "Succeeded to run e2e test."
 
 exit 0
