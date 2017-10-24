84148c9ca6e76eaf4668c50d89933a8935a891cf
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-10-24 06:09:47 -0700

    Fix some script

--- ci/00_gitlab-runner/ci_test/test_script.sh
+++ ci/00_gitlab-runner/ci_test/test_script.sh
@@ -4,7 +4,7 @@ SLACK_API_TOKEN=`cat ./slack_api_token`
 RETURN_CODE=0
 LAST_LINE=`tail -n1 ./github_history.txt`
 MESSAGE="Succeeded to test the remora commit ${LAST_LINE}"
-TEMPFILE=`mktemp remora.log-`
+TEMPFILE=`mktemp remora.log-XXXX`
 
 ./run_test.sh | tee ${TEMPFILE}
 if [ $? -ne 0 ]; then
@@ -15,4 +15,4 @@ fi
 curl -XPOST -d "token=${SLACK_API_TOKEN}" -d "channel=#general" -d "text=${MESSAGE}" -d "username=remora-bot" "https://slack.com/api/chat.postMessage"
 
 rm ${TEMPFILE}
-return ${RETURN_CODE}
+exit ${RETURN_CODE}
