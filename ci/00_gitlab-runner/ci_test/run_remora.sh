93501ad4ed402548822915d3d0a7ab338cf59de8
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-10-24 04:10:47 -0700

    Add debug info

--- ci/00_gitlab-runner/ci_test/run_remora.sh
+++ ci/00_gitlab-runner/ci_test/run_remora.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
 # Operate remora!!
+echo "Start to pip install.."
 pip3 install -r requirements.txt
 if [ $? -ne 0 ]; then
 	echo "Failed to pip install"
 	exit 1
 fi
 
+echo "Start to fab cluster render.."
 fab cluster render
 if [ $? -ne 0 ]; then
 	echo "Failed to fab cluster render"
 	exit 1
 fi
 
+echo "Start to fab cluster install.kubelet.."
 fab cluster install.kubelet
 if [ $? -ne 0 ]; then
 	echo "Failed to fab cluster install.kubelet"
 	exit 1
 fi
 
+echo "Start to fab cluster install.etcd.."
 fab cluster install.etcd
 if [ $? -ne 0 ]; then
 	echo "Failed to fab cluster install.etcd"
 	exit 1
 fi
 
+echo "Start to fab cluster install.bootstrap.."
 fab cluster install.bootstrap
 if [ $? -ne 0 ]; then
 	echo "Failed to fab cluster install.bootstrap"
 	exit 1
 fi
 
+echo "Start to fab cluster install.kubernetes.."
 fab cluster install.kubernetes
 if [ $? -ne 0 ]; then
 	echo "Failed to fab cluster install.kubernetes"
 	exit 1
 fi
+
+echo "remora is done!!"
 
 exit 0
