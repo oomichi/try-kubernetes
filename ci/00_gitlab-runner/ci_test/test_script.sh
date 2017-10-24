ae280630535dd8d6fad0e808471d1eba9227d807
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-10-24 05:56:13 -0700

    Show log on gitlab console

--- ci/00_gitlab-runner/ci_test/test_script.sh
+++ ci/00_gitlab-runner/ci_test/test_script.sh
@@ -4,11 +4,15 @@ SLACK_API_TOKEN=`cat ./slack_api_token`
 RETURN_CODE=0
 LAST_LINE=`tail -n1 ./github_history.txt`
 MESSAGE="Succeeded to test the remora commit ${LAST_LINE}"
+TEMPFILE=`mktemp remora.log-`
 
-DETAIL=`./run_test.sh`
+./run_test.sh | tee ${TEMPFILE}
 if [ $? -ne 0 ]; then
 	RETURN_CODE=1
+	DETAIL=`cat ${TEMPFILE}`
 	MESSAGE="Failed to test the commit ${LAST_LINE}. ${DETAIL}"
 fi
 curl -XPOST -d "token=${SLACK_API_TOKEN}" -d "channel=#general" -d "text=${MESSAGE}" -d "username=remora-bot" "https://slack.com/api/chat.postMessage"
 
+rm ${TEMPFILE}
+return ${RETURN_CODE}
