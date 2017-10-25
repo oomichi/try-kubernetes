90ddd31382432bd8ebeccfac7042d334117fe405
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-10-25 13:52:24 -0700

    Add IP address for e2e

--- ci/00_gitlab-runner/ci_test/run_e2e.sh
+++ ci/00_gitlab-runner/ci_test/run_e2e.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd 'dirname $0'
+
+IP_E2E=`openstack server show e2e -c addresses -f value | sed s/"provider="//`
+cp -f ./hosts_e2e.org ./hosts_e2e
+sed -i s/"IP_E2E"/"${IP_E2E}"/g  ./hosts_e2e
+
 echo "Start to run e2e test.."
 ansible-playbook -vvvv -i ./hosts_e2e run_e2e_test.yaml
 if [ $? -ne 0 ]; then
