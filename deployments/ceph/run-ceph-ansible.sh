ec26fa0646b27fe56e0cfed748a7107318d85a9d
Kenichi Omichi <ken-oomichi@wx.jp.nec.com>
2020-05-26 17:35:10 +0000

    Add prefix for deployment values

--- deployments/ceph/run-ceph-ansible.sh
+++ deployments/ceph/run-ceph-ansible.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 
-MONITOR_INTERFACE=${MONITOR_INTERFACE:-"eth0"}
-PUBLIC_NETWORK=${PUBLIC_NETWORK}
+CEPH_MONITOR_INTERFACE=${CEPH_MONITOR_INTERFACE:-"eth0"}
+CEPH_PUBLIC_NETWORK=${CEPH_PUBLIC_NETWORK}
 
-if [ "${PUBLIC_NETWORK}" = "" ]; then
+if [ "${CEPH_PUBLIC_NETWORK}" = "" ]; then
 	echo 'Need to specify public network address like:'
 	echo ''
-	echo '  $ export PUBLIC_NETWORK="10.0.0.0/24"'
+	echo '  $ export CEPH_PUBLIC_NETWORK="10.0.0.0/24"'
 	echo '  $ ./run-ceph-ansible.sh'
 	exit 1
 fi
-if [ "${MON_NODES}" = "" ]; then
+if [ "${CEPH_MON_NODES}" = "" ]; then
 	echo 'Need to specify monitor nodes like:'
 	echo ''
-	echo '  $ export MON_NODES="ceph-mon01 ceph-mon02"'
+	echo '  $ export CEPH_MON_NODES="ceph-mon01 ceph-mon02"'
 	echo '  $ ./run-ceph-ansible.sh'
 	exit 1
 fi
-if [ "${MGR_NODES}" = "" ]; then
+if [ "${CEPH_MGR_NODES}" = "" ]; then
 	echo 'Need to specify manage nodes like:'
 	echo ''
-	echo '  $ export MGR_NODES="ceph-mgr01 ceph-mgr02"'
+	echo '  $ export CEPH_MGR_NODES="ceph-mgr01 ceph-mgr02"'
 	echo '  $ ./run-ceph-ansible.sh'
 	exit 1
 fi
-if [ "${OSD_NODES}" = "" ]; then
+if [ "${CEPH_OSD_NODES}" = "" ]; then
 	echo 'Need to specify osd nodes like:'
 	echo ''
-	echo '  $ export OSD_NODES="ceph-osd01 ceph-osd02 ceph-osd03"'
+	echo '  $ export CEPH_OSD_NODES="ceph-osd01 ceph-osd02 ceph-osd03"'
 	echo '  $ ./run-ceph-ansible.sh'
 	exit 1
 fi
-if [ "${DATA_DEVICES}" = "" ]; then
+if [ "${CEPH_DATA_DEVICES}" = "" ]; then
 	echo 'Need to specify storage devices on osd nodes.'
 	echo 'NOTE: All osd nodes should have the same specified storage devices.'
 	echo 'NOTE: These storage devices should not be logical devices(/dev/loop0, etc.)'
 	echo ''
-	echo '  $ export DATA_DEVICES="/dev/sdb"'
+	echo '  $ export CEPH_DATA_DEVICES="/dev/sdb"'
 	echo '  $ ./run-ceph-ansible.sh'
 	exit 1
 fi
 
 # TODO: Add all node checks
-for node in ${MON_NODES}; do
+for node in ${CEPH_MON_NODES}; do
 	ping -c 1 ${node}
 	if [ $? -ne 0 ]; then
-		echo "Node ${node} in MON_NODES is not reachable"
+		echo "Node ${node} in CEPH_MON_NODES is not reachable"
 		exit 1
 	fi
 done
@@ -54,15 +54,15 @@ done
 rm -f ./hosts
 touch ./hosts
 echo         "[mons]"  >> ./hosts
-for node in ${MON_NODES}; do
+for node in ${CEPH_MON_NODES}; do
 	echo "${node}" >> ./hosts
 done
 echo         "[mgrs]"  >> ./hosts
-for node in ${MGR_NODES}; do
+for node in ${CEPH_MGR_NODES}; do
 	echo "${node}" >> ./hosts
 done
 echo         "[osds]"  >> ./hosts
-for node in ${OSD_NODES}; do
+for node in ${CEPH_OSD_NODES}; do
 	echo "${node}" >> ./hosts
 done
 
@@ -70,7 +70,7 @@ done
 rm -f ./group_vars/osds.yml
 touch ./group_vars/osds.yml
 echo         "devices:"   >> ./group_vars/osds.yml
-for dev in ${DATA_DEVICES}; do
+for dev in ${CEPH_DATA_DEVICES}; do
 	echo "  - ${dev}" >> ./group_vars/osds.yml
 done
 
@@ -89,9 +89,9 @@ git checkout remotes/origin/stable-4.0
 
 sudo pip3 install -r requirements.txt
 
-cp ${PATH_THIS_SCRIPT}/files/all.yml                 group_vars/all.yml
-sed -i s/"MONITOR_INTERFACE"/"${MONITOR_INTERFACE}"/ group_vars/all.yml
-sed -i s@"PUBLIC_NETWORK"@"${PUBLIC_NETWORK}"@       group_vars/all.yml  # PUBLIC_NETWORK contains /, so we need to use another char here.
+cp ${PATH_THIS_SCRIPT}/files/all.yml                           group_vars/all.yml
+sed -i s/"CEPH_MONITOR_INTERFACE"/"${CEPH_MONITOR_INTERFACE}"/ group_vars/all.yml
+sed -i s@"CEPH_PUBLIC_NETWORK"@"${CEPH_PUBLIC_NETWORK}"@       group_vars/all.yml  # CEPH_PUBLIC_NETWORK contains /, so we need to use another char here.
 
 cp site.yml.sample site.yml
 
--- deployments/kubespray/run-kubespray.sh
+++ deployments/kubespray/run-kubespray.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-KUBE_VERSION=${KUBE_VERSION:-"v1.17.5"}
+K8S_VERSION=${K8S_VERSION:-"v1.17.5"}
 
-if [ "${IPS_NODES}" = "" ]; then
+if [ "${K8S_NODES}" = "" ]; then
 	echo 'Need to specify IP addresses of target nodes like:'
-	echo '$ IPS_NODES="192.168.1.102 192.168.1.135" ./run-kubespray.sh'
+	echo '$ K8S_NODES="192.168.1.102 192.168.1.135" ./run-kubespray.sh'
 	exit 1
 fi
 
-declare -a IPS=(${IPS_NODES})
+declare -a IPS=(${K8S_NODES})
 
 # Enable error handling
 set -e
@@ -27,7 +27,7 @@ CONFIG_FILE=inventory/sample/hosts.yaml python3 contrib/inventory_builder/invent
 
 sed -i s/"^metrics_server_enabled: false"/"metrics_server_enabled: true"/ inventory/sample/group_vars/k8s-cluster/addons.yml
 sed -i s/"^ingress_nginx_enabled: false"/"ingress_nginx_enabled: true"/   inventory/sample/group_vars/k8s-cluster/addons.yml
-sed -i s/"^kube_version: v1.17.5"/"kube_version: ${KUBE_VERSION}"/        inventory/sample/group_vars/k8s-cluster/k8s-cluster.yml
+sed -i s/"^kube_version: v1.17.5"/"kube_version: ${K8S_VERSION}"/         inventory/sample/group_vars/k8s-cluster/k8s-cluster.yml
 sed -i s/"^# kubeconfig_localhost: false"/"kubeconfig_localhost: true"/   inventory/sample/group_vars/k8s-cluster/k8s-cluster.yml
 sed -i s/"^kube_network_plugin: calico"/"kube_network_plugin: flannel"/   inventory/sample/group_vars/k8s-cluster/k8s-cluster.yml
 sed -i s/"^override_system_hostname: true"/"override_system_hostname: false"/ roles/bootstrap-os/defaults/main.yml
