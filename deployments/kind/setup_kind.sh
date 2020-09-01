7d9c476255cdd99fdb64cdc4de3787cc66e86f31
Kenichi Omichi <ken-oomichi@wx.jp.nec.com>
2020-09-01 17:27:05 +0000

    Use binary kind instead of golang one

--- deployments/kind/setup_kind.sh
+++ deployments/kind/setup_kind.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
+set -e
 
 CLUSTER_NAME=$1
 if [ "${CLUSTER_NAME}" = "" ]; then
 	CLUSTER_NAME="kind"
 fi
 
-egrep "CentOS|RHEL" /etc/os-release
-if [ $? -eq 0 ]; then
-	CENTOS=TRUE
-else
-	CENTOS=""
-fi
+sudo chmod 666 /var/run/docker.sock
 
-go version
-if [ $? -ne 0 ]; then
-	set -e
-	echo "Installing golang.."
-	wget https://golang.org/dl/go1.14.7.linux-amd64.tar.gz
-	sudo tar -C /usr/local/ -xzf go1.14.7.linux-amd64.tar.gz
-	rm go1.14.7.linux-amd64.tar.gz
-	export PATH=$PATH:/usr/local/go/bin:${HOME}/go/bin
-	echo "export PATH=$PATH:/usr/local/go/bin:${HOME}/go/bin" >> ${HOME}/.bashrc
-	sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
-	mkdir ${HOME}/go
-	export GOPATH=${HOME}/go
-	echo "export GOPATH=${HOME}/go" >> ${HOME}/.bashrc
+# Get kind command
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/
 
-	if [ -z "${CENTOS}" ]; then
-		sudo apt-get install -y docker.io gcc make
-	else
-		sudo yum install -y docker docker-client gcc make
-		sudo systemctl start docker
-	fi
-	sudo chmod 666 /var/run/docker.sock
-fi
-
-set -e
-GO111MODULE="on" go get sigs.k8s.io/kind@v0.8.1
 set +e
-
 kubectl version
 if [ $? -eq 127 ]; then
 	curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
@@ -47,6 +22,6 @@ if [ $? -eq 127 ]; then
 fi
 
 # Cleanup the previous one
-kind create delete --name ${CLUSTER_NAME}
+kind delete cluster --name ${CLUSTER_NAME}
 
 kind create cluster --name ${CLUSTER_NAME}
