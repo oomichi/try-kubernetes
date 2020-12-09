7d7b006001478fcb2bce95155ebbd3e5aad28072
Kenichi Omichi <ken-oomichi@wx.jp.nec.com>
2020-12-09 22:08:28 +0000

    Add create-container-image-tar.sh

---
+++ deployments/kubespray/create-container-image-tar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+IMAGES=$(kubectl describe pods --all-namespaces | grep " Image:" | awk '{print $2}' | sort | uniq)
+
+rm -f  container-images.tar.gz
+rm -rf container-images/
+mkdir  container-images/
+cd     container-images/
+
+for image in ${IMAGES}
+do
+	FILE_NAME="$(echo ${image} | awk -F"/" '{print $2}' | sed s/":"/"-"/g)".tar
+	sudo docker pull ${image}
+	sudo docker save -o ${FILE_NAME}  ${image}
+done
+
+tar -zcvf container-images.tar.gz container-images/
