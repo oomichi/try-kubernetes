a4142c31a09e20e4ab5b3cbfc8e9f9d850dd1490
Kenichi Omichi <ken-oomichi@wx.jp.nec.com>
2021-01-30 00:05:39 +0000

    Add local-pv.yaml

--- deployments/kubespray/create-local-pv.sh
+++ deployments/kubespray/create-local-pv.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
-MOUNT_NAME="${1}"
+PV_NAME="${1}"
 PV_SIZE_GB="${2}"
 
-if [ -z "${MOUNT_NAME}" ] || [ -z "${PV_SIZE_GB}" ]; then
-	echo "Need to specify MOUNT_NAME and PV_SIZE_GB like:"
+if [ -z "${PV_NAME}" ] || [ -z "${PV_SIZE_GB}" ]; then
+	echo "Need to specify PV_NAME and PV_SIZE_GB like:"
 	echo "./create-local-pv.sh foo-mount 10"
 	exit 1
 fi
 
 # Ignore error of mkdir here because of the existing directory case.
 sudo mkdir /var/disks
-sudo truncate /var/disks/${MOUNT_NAME} --size ${PV_SIZE_GB}G
+sudo truncate /var/disks/${PV_NAME} --size ${PV_SIZE_GB}G
 if [ $? -ne 0 ]; then
-	echo "Failed to create a file of /var/disks/${MOUNT_NAME}"
+	echo "Failed to create a file of /var/disks/${PV_NAME}"
 	exit 1
 fi
 
-sudo mkfs.ext4 -F /var/disks/${MOUNT_NAME}
+sudo mkfs.ext4 -F /var/disks/${PV_NAME}
 if [ $? -ne 0 ]; then
-	echo "Failed to create ext4fs of /var/disks/${MOUNT_NAME}"
+	echo "Failed to create ext4fs of /var/disks/${PV_NAME}"
 	exit 1
 fi
 
-sudo mkdir /mnt/disks/${MOUNT_NAME}
+sudo mkdir /mnt/disks/${PV_NAME}
 if [ $? -ne 0 ]; then
-	echo "Failed to create directory of /mnt/disks/${MOUNT_NAME}"
+	echo "Failed to create directory of /mnt/disks/${PV_NAME}"
 	exit 1
 fi
 
-sudo mount /var/disks/${MOUNT_NAME} /mnt/disks/${MOUNT_NAME}
+sudo mount /var/disks/${PV_NAME} /mnt/disks/${PV_NAME}
 if [ $? -ne 0 ]; then
-	echo "Failed to mount directory of /mnt/disks/${MOUNT_NAME}"
+	echo "Failed to mount directory of /mnt/disks/${PV_NAME}"
 	exit 1
 fi
 
-sudo echo '/var/disks/${MOUNT_NAME}  /mnt/disks/${MOUNT_NAME}  ext4  defaults  0  0' | sudo tee -a /etc/fstab
+sudo echo '/var/disks/${PV_NAME}  /mnt/disks/${PV_NAME}  ext4  defaults  0  0' | sudo tee -a /etc/fstab
 if [ $? -ne 0 ]; then
-	echo "Failed to write /etc/fstab for ${MOUNT_NAME}"
+	echo "Failed to write /etc/fstab for ${PV_NAME}"
+	exit 1
+fi
+
+cp ./yaml/local-pv.yaml            /tmp/local-pv.yaml
+sed -i s/PV_NAME/${PV_NAME}/g      /tmp/local-pv.yaml
+sed -i s/PV_SIZE_GB/${PV_SIZE_GB}/ /tmp/local-pv.yaml
+kubectl create -f                  /tmp/local-pv.yaml
+if [ $? -ne 0 ]; then
+	echo "Failed to create a persistent volume"
 	exit 1
 fi
