331292f9b061a2c106f6ac5c58c42c0ad1ec6f84
Kenichi Omichi <ken-oomichi@wx.jp.nec.com>
2022-04-08 00:06:01 +0000

    Update create-env.sh

--- deployments/vagrant-kubespray/create-env.sh
+++ deployments/vagrant-kubespray/create-env.sh
@@ -6,9 +6,17 @@ set -e
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt update
-sudo apt install vagrant
+sudo apt -y install vagrant
 
 # Install vagrant-libvirt
-sudo apt install libvirt-dev build-essential
+sudo apt -y install libvirt-dev build-essential
 vagrant plugin install vagrant-libvirt
 
+cd
+python3 -m venv ./venv-kubespray
+source venv-kubespray/bin/activate
+
+git clone https://github.com/kubernetes-sigs/kubespray
+cd kubespray
+pip3 install -r requirements.txt
+vagrant up
