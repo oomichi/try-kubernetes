88c7d1fbf49d89a57fd1bee2757913810114de60
Kenichi Omichi <ken-oomichi@wx.jp.nec.com>
2022-04-25 20:12:12 +0000

    Make run-vagrant.sh runable again and again

--- deployments/vagrant-kubespray/run-vagrant.sh
+++ deployments/vagrant-kubespray/run-vagrant.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+vagrant plugin install vagrant-libvirt
+
 cd
+rm -rf ./venv-kubespray
 python3 -m venv ./venv-kubespray
 source venv-kubespray/bin/activate
 
-git clone https://github.com/kubernetes-sigs/kubespray
+if [ ! -d ./kubespray ]; then
+    git clone https://github.com/kubernetes-sigs/kubespray
+fi
 cd kubespray
 pip3 install -r requirements.txt
 vagrant up
