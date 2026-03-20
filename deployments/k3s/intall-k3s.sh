626d5eb8db2ed9ab897935e368788b48341bf315
Kenichi Omichi <ken1ohmichi@gmail.com>
2026-03-20 06:57:04 +0000

    Add keycloak.yaml
    from https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/refs/heads/main/kubernetes/keycloak.yaml

--- deployments/k3s/intall-k3s.sh
+++ deployments/k3s/intall-k3s.sh
@@ -5,17 +5,19 @@ if [ $? -ne 0 ]; then
 	set -e
 	curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="666" sh -
 	set +e
+	mkdir ~/.kube
+	ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
 fi
 
-k3s kubectl get nodes
+kubectl get nodes
 if [ $? -ne 0 ]; then
 	echo "Failed to install k3s."
 	exit 1
 fi
 
-k3s kubectl -n kube-system get pods | grep local-path-provisioner
+kubectl -n kube-system get pods | grep local-path-provisioner
 if [ $? -ne 0 ]; then
-	k3s kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.35/deploy/local-path-storage.yaml
+	kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.35/deploy/local-path-storage.yaml
 fi
 
 echo "Succeeded to install k3s."
